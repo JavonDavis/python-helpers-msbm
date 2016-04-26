dd2246e930c3d1fa3b444e5d13a07955d6a8c68c
JA-VON <javonldavis14@gmail.com>
2016-04-26 07:39:13 +0000

    updated with trained model, remember when training to use drp_fuseki

--- maui-exec.sh
+++ maui-exec.sh
@@ -1,3 +1,3 @@
-java -Xmx1024m -jar maui2.jar train -l training/ -m output_model -v drp.rdf -f skos
+java -Xmx1024m -jar maui3.jar train -l training/ -m output_model -v drp.rdf -f skos
 
-java -Xmx1024m -jar maui2.jar run input.txt -m output_model -v drp.rdf -f skos
+java -Xmx1024m -jar maui3.jar run input.txt -m output_model -v drp.rdf -f skos
